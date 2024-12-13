2838deb9991aa837965caa9a007e80e3a66ce1a1
VengateshGanesan <vengateshtendulkar@gmail.com>
2024-12-13 13:26:14 +0530

    Commented Running server

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
 module.exports = app;
